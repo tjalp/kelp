6bcbdcb6815697d898fee329c1014df86a822384
tjalp <tiesalkemade@gmail.com>
2022-06-09 18:10:43 +0200

    feat: init

---
+++ src/main/kotlin/net/tjalp/kelp/Kelp.kt
@@ -0,0 +1,15 @@
+package net.tjalp.kelp
+
+import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+@Suppress("UNUSED")
+object Kelp : ModInitializer {
+    const val MOD_ID = "kelp"
+    val logger: Logger = LogManager.getLogger()
+
+    override fun onInitialize() {
+
+    }
+}
